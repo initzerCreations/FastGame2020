aa10452dcb4bd2065ee6e74359a241d3431b5fbc
Jacobo Casas Ramos <jacobo.casas.ramos@gmail.com>
2020-10-10 19:11:32 +0200

    Intro y otras mejoras

--- MainMenu.gd
+++ MainMenu.gd
@@ -1,8 +1,12 @@
 extends Control
 
+var time = 0.0
+
 func _process(delta):
+	time += delta
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
-	if Input.is_action_just_pressed("exit"):
-		get_tree().quit()
-	if Input.is_action_just_pressed("use_object"):
-		get_tree().change_scene("res://World.tscn")
+	if time > 2.0:
+		if Input.is_action_just_pressed("exit"):
+			get_tree().quit()
+		if Input.is_action_just_pressed("use_object"):
+			get_tree().change_scene("res://World.tscn")
